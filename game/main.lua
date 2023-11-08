fa08ee21d95a779cd218c78b11711d7396e18e22
maxilamano <maxilamano@gmail.com>
2023-11-08 14:53:03 -0300

    sprites!

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,8 @@
+local sprPlayer ={
+    love.graphics.newImage("sprites/player/playerOff.png"),
+    love.graphics.newImage("sprites/player/playerOn.png")
+}
+qdPlayer = love.graphics.newQuad(0,0,32,16,sprPlayer[1]);
 function love.draw(screen)
     local Palette = {
         {5/255, 31/255, 57/255, 1},
@@ -10,7 +15,7 @@ function love.draw(screen)
     for i, id in ipairs(touches) do
         local x, y = love.touch.getPosition(id)
         if screen == "bottom" then
-            love.graphics.circle("fill", x, y, 10) -- render bottom screen
+            love.graphics.draw(sprPlayer[1], qdPlayer, x-16, y-8,0 ,0, 2, 2) -- render bottom screen
         end
     end
     love.graphics.print('Hello World!', 200, 120)
